ccfbf90ee809485aadc485687d62460162879ff4
Mathieu Jobin <mathieu@justbudget.com>
2022-04-17 10:15:10 +0900

    rails 4.2 requires to store .lock file so bundler 1.17 is selected

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,3 @@
-require 'coveralls'
-Coveralls.wear!
-
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
 
@@ -11,6 +8,27 @@ require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 require 'minitest/autorun'
 require 'capybara/rails'
 require 'active_support/core_ext/kernel/reporting'
+
+if Rails.version > '5'
+  require 'coveralls'
+  Coveralls.wear!
+end
+
+# Hack to run tests with Rails 4.2 and Ruby 2.6
+if RUBY_VERSION >= '2.6.0'
+  if Rails.version < '5'
+    # rubocop:disable Style/CommentAnnotation
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+    # rubocop:enable Style/CommentAnnotation
+  end
+end
 
 Rails.backtrace_cleaner.remove_silencers!
 
